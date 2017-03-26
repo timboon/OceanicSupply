8a88620b7969f23de1c251ea460b3b2aada09a3b
Oliver <blind118@hotmail.com>
2017-03-26 04:17:21 +0000

    added config in application.rb for paperclip to work with s3

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,18 @@ module Railsapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    
+    config.paperclip_defaults = {
+      storage: :s3,
+      s3_host_name: 's3-ap-southeast-2.amazonaws.com',
+      s3_credentials: {
+        bucket: ENV.fetch('S3_BUCKET_NAME'),
+        access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+        secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+        s3_region: ENV.fetch('AWS_REGION'),
+      }
+    }
+    
   end
 end
